057d8bfab509653fae4bb50ebe5f056269009f33
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-02-11 20:41:17 -0200

    Add Optional & ItemUtil class

---
+++ src/Common/Optional.cs
@@ -0,0 +1,106 @@
+﻿/*
+ *  This file is part of uEssentials project.
+ *      https://uessentials.github.io/
+ *
+ *  Copyright (C) 2015-2016  Leonardosc
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System;
+
+namespace Essentials.Common
+{
+    /// <summary>
+    ///  A container object which may or may not contain a non-null value. 
+    ///  If a value is present, <see cref="IsPresent"/> will return <code>true</code> and 
+    /// <see cref="Value"/> will return the value.
+    /// </summary>
+    public class Optional<T>
+    {
+        private static readonly Optional<T> EMPTY = new Optional<T>( default(T) );
+        private readonly T _value;
+
+        public bool IsPresent { get; }
+
+        public bool IsAbsent { get; }
+
+        public T Value
+        {
+            get
+            {
+                if ( _value == null )
+                {
+                    throw new InvalidOperationException( "No value present." );
+                }
+
+                return _value;
+            }
+        }
+
+        private Optional( T value )
+        {
+            _value = value;
+
+            IsPresent = value != null;
+            IsAbsent  = value == null;
+        }
+
+        public void IfPresent( Action<T> consumer )
+        {
+            Preconditions.NotNull( consumer, "consumer cannot be null" );
+
+            if ( IsPresent )
+            {
+                consumer( Value );
+            }
+        }
+
+        public void IfAbsent( Action<T> consumer )
+        {
+            Preconditions.NotNull( consumer, "consumer cannot be null" );
+
+            if ( IsAbsent )
+            {
+                consumer( Value );
+            }
+        }
+
+        public T OrElse( T value )
+        {
+            return IsPresent ? Value : value;
+        }
+
+        public static Optional<T> Of( T value )
+        {
+            if ( value == null )
+            {
+                throw new ArgumentNullException( nameof(value), "value cannot be null" );
+            }
+
+            return new Optional<T>( value );
+        }
+
+        public static Optional<T> OfNullable( T value )
+        {
+            return new Optional<T>( value );
+        }
+
+        public static Optional<T> Empty()
+        {
+            return EMPTY;
+        }
+    }
+}
---
+++ src/Common/Util/ItemUtil.cs
@@ -0,0 +1,74 @@
+﻿/*
+ *  This file is part of uEssentials project.
+ *      https://uessentials.github.io/
+ *
+ *  Copyright (C) 2015-2016  Leonardosc
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System;
+using System.Linq;
+using SDG.Unturned;
+
+namespace Essentials.Common.Util
+{
+    public static class ItemUtil
+    {
+        public static Optional<ItemAsset> GetItem( ushort id )
+        {
+            return Optional<ItemAsset>.OfNullable( (ItemAsset) Assets.find( EAssetType.ITEM, id ) );
+        }
+
+        public static Optional<ItemAsset> GetItem( string name )
+        {
+            if ( name == null )
+            {
+                return Optional<ItemAsset>.Empty();
+            }
+
+            ushort id;
+
+            if ( ushort.TryParse( name, out id ) )
+            {
+                return GetItem( id );
+            }
+
+            var asset = (
+                from it in Assets.find( EAssetType.ITEM ) 
+                let itemAsset = (ItemAsset) it
+                where itemAsset.Name != null
+                where itemAsset.Name.ToLower().Contains( name.ToLower() )
+                select itemAsset
+            ).FirstOrDefault();
+
+            return Optional<ItemAsset>.OfNullable( asset );
+        }
+
+        public static Optional<T> GetItemAs<T>( string name ) where T : ItemAsset
+        {
+            var optItem = GetItem( name );
+
+            return optItem.IsPresent ? Optional<T>.Of( optItem.Value as T ) : Optional<T>.Empty();
+        }
+
+        public static Optional<T> GetItemAs<T>( ushort id ) where T : ItemAsset
+        {
+            var optItem = GetItem( id );
+
+            return optItem.IsPresent ? Optional<T>.Of( optItem.Value as T ) : Optional<T>.Empty();
+        }
+    }
+}
