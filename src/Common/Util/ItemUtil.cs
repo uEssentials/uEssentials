5e8d8ed599a5e1fe4a625723c0ad15945436e45f
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-02-13 14:34:53 -0200

    Add lot of methods in ItemUtil
    It will be helpful in /createkit

--- src/Common/Util/ItemUtil.cs
+++ src/Common/Util/ItemUtil.cs
@@ -19,7 +19,11 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Essentials.Kits;
+using Rocket.Unturned.Items;
 using SDG.Unturned;
 
 namespace Essentials.Common.Util
@@ -68,6 +72,144 @@ namespace Essentials.Common.Util
             var optItem = GetItem( id );
 
             return optItem.IsPresent ? Optional<T>.Of( optItem.Value as T ) : Optional<T>.Empty();
+        }
+
+        public static Optional<Attachment> GetWeaponAttachment( Item weaponItem, AttachmentType type )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return Optional<Attachment>.Empty();
+            }
+
+            int[] indexes;
+
+            switch (type)
+            {
+                case AttachmentType.SIGHT:
+                    indexes = new[] {0x0, 0x1, 0xD};
+                    break;
+
+                case AttachmentType.TATICAL:
+                    indexes = new[] { 0x2, 0x3, 0xE };
+                    break;
+
+                case AttachmentType.GRIP:
+                    indexes = new[] { 0x4, 0x5, 0xF };
+                    break;
+
+                case AttachmentType.BARREL:
+                    indexes = new[] { 0x6, 0x7, 0x10 };
+                    break;
+
+                case AttachmentType.MAGAZINE:
+                    indexes = new[] { 0x8, 0x9, 0x11 };
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException( nameof( type ), type, null );
+            }
+
+            var attachDurability = weaponItem.Metadata[indexes[2]];
+            var attachId = BitConverter.ToUInt16( new [] {
+                weaponItem.Metadata[indexes[0]],
+                weaponItem.Metadata[indexes[1]]
+            }, 0 );
+
+            return Optional<Attachment>.Of( new Attachment( attachId, attachDurability ) );
+        }
+
+        public static Optional<EFiremode> GetWeaponFiremode( Item weaponItem )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return Optional<EFiremode>.Empty();
+            }
+
+            return Optional<EFiremode>.OfNullable( (EFiremode) weaponItem.Metadata[0xB] );
+        }
+
+        public static byte GetWeaponAmmo( Item weaponItem )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return 0;
+            }
+
+            return weaponItem.Metadata[0xA];
+        }
+
+        public static void SetWeaponAttachment( Item weaponItem, AttachmentType type, Attachment attach )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return;
+            }
+
+            switch (type)
+            {
+                case AttachmentType.SIGHT:
+                    AssembleAttach( weaponItem, new[] { 0x0, 0x1, 0xD }, attach );
+                    break;
+
+                case AttachmentType.TATICAL:
+                    AssembleAttach( weaponItem, new[] { 0x2, 0x3, 0xE }, attach );
+                    break;
+
+                case AttachmentType.GRIP:
+                    AssembleAttach( weaponItem, new[] { 0x4, 0x5, 0xF }, attach );
+                    break;
+
+                case AttachmentType.BARREL:
+                    AssembleAttach( weaponItem, new[] { 0x6, 0x7, 0x10 }, attach );
+                    break;
+
+                case AttachmentType.MAGAZINE:
+                    AssembleAttach( weaponItem, new[] { 0x8, 0x9, 0x11 }, attach );
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException( nameof( type ), type, null );
+            }
+        }
+
+        public static void SetWeaponFiremode( Item weaponItem, EFiremode firemode )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return;
+            }
+
+            weaponItem.Metadata[0xB] = (byte) firemode;
+        }
+
+        public static void SetWeaponAmmo( Item weaponItem, byte ammo )
+        {
+            if ( weaponItem.Metadata.Length < 18 )
+            {
+                return;
+            }
+
+            weaponItem.Metadata[0xA] = ammo;   
+        }
+
+        private static void AssembleAttach( Item item, IList<int> indexes, Attachment attach )
+        {
+            if ( attach == null || attach.AttachmentId == 0 ) return;
+
+            var attachIdBytes = BitConverter.GetBytes( attach.AttachmentId );
+
+            item.Metadata[indexes[0]] = attachIdBytes[0];
+            item.Metadata[indexes[1]] = attachIdBytes[1];
+            item.Metadata[indexes[2]] = attach.Durability;
+        }
+
+        public enum AttachmentType
+        {
+            SIGHT,
+            BARREL,
+            GRIP,
+            TATICAL,
+            MAGAZINE
         }
     }
 }
