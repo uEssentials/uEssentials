a5316b2c5390898d9203f4385fbe574b739d46a3
Leonardo Santos <devleeo@outlook.com>
2016-04-08 19:31:46 -0300

    1.1.9.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.1.9.1" )]
-[assembly: AssemblyFileVersion( "1.1.9.1" )]
+[assembly: AssemblyVersion( "1.1.9.2" )]
+[assembly: AssemblyFileVersion( "1.1.9.2" )]
--- src/Core/EssCore.cs
+++ src/Core/EssCore.cs
@@ -62,9 +62,9 @@ namespace Essentials.Core
                 - Improve event system to avoid duplicate events.
         */
         
-        internal const string                         PLUGIN_VERSION              = "1.1.9.1";
-        internal const string                         ROCKET_VERSION              = "4.9.3.1";
-        internal const string                         UNTURNED_VERSION            = "3.14.11.2";
+        internal const string                         PLUGIN_VERSION              = "1.1.9.2";
+        internal const string                         ROCKET_VERSION              = "4.9.4.0";
+        internal const string                         UNTURNED_VERSION            = "3.14.12.1";
         
         internal static EssCore                       Instance                    { get; set; }
         
