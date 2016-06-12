48013ed9e548a0c964410d66cb3d597e5325d5bb
Leonardo Santos <devleeo@outlook.com>
2016-06-12 18:31:35 -0300

    1.2.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.2.2.3" )]
-[assembly: AssemblyFileVersion( "1.2.2.3" )]
+[assembly: AssemblyVersion( "1.2.3.0" )]
+[assembly: AssemblyFileVersion( "1.2.3.0" )]
