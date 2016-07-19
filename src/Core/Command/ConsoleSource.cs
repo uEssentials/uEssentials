2c7d9c5dfeed8ba571e6ba2fc3f8b9a85e46e6d2
Leonardo Santos <devleeo@outlook.com>
2016-07-19 02:36:59 -0200

    Translate non-ascii aeiou to ascii.

--- src/Core/Command/ConsoleSource.cs
+++ src/Core/Command/ConsoleSource.cs
@@ -61,7 +61,7 @@ namespace Essentials.Core.Command {
         public void SendMessage(object message, Color color) {
             var oldColor = Console.ForegroundColor;
             Console.ForegroundColor = ColorUtil.UnityColorToConsoleColor(color);
-            Console.WriteLine(message);
+            Console.WriteLine(message is string ? AeiouToAscii((string) message) : message);
             Console.ForegroundColor = oldColor;
         }
 
@@ -77,6 +77,56 @@ namespace Essentials.Core.Command {
             return DisplayName;
         }
 
+        private /*unsafe*/ static string AeiouToAscii(string str) {
+            var chars = str.ToCharArray();
+
+            for (var i = 0; i < chars.Length; i++) {
+                if (chars[i] >= 224 && chars[i] <= 229) chars[i] = 'a';
+                if (chars[i] >= 192 && chars[i] <= 197) chars[i] = 'A';
+
+                if (chars[i] >= 232 && chars[i] <= 235) chars[i] = 'e';
+                if (chars[i] >= 200 && chars[i] <= 203) chars[i] = 'E';
+
+                if (chars[i] >= 236 && chars[i] <= 239) chars[i] = 'i';
+                if (chars[i] >= 204 && chars[i] <= 207) chars[i] = 'I';
+
+                if (chars[i] >= 242 && chars[i] <= 246) chars[i] = 'o';
+                if (chars[i] >= 210 && chars[i] <= 214) chars[i] = 'O';
+
+                if (chars[i] >= 249 && chars[i] <= 252) chars[i] = 'u';
+                if (chars[i] >= 218 && chars[i] <= 220) chars[i] = 'U';
+
+                if (chars[i] == 231) chars[i] = 'c';
+                if (chars[i] == 199) chars[i] = 'C';
+            }
+
+            /*
+            There are no performance diff... :C
+
+            for (var i = 0; i < chars.Length; i++) {
+                fixed (char* cur = &chars[i]) {
+                    if (*cur >= 224 && *cur <= 229) *cur = 'a';
+                    if (*cur >= 192 && *cur <= 197) *cur = 'A';
+
+                    if (*cur >= 232 && *cur <= 235) *cur = 'e';
+                    if (*cur >= 200 && *cur <= 203) *cur = 'E';
+
+                    if (*cur >= 236 && *cur <= 239) *cur = 'i';
+                    if (*cur >= 204 && *cur <= 207) *cur = 'I';
+
+                    if (*cur >= 242 && *cur <= 246) *cur = 'o';
+                    if (*cur >= 210 && *cur <= 214) *cur = 'O';
+
+                    if (*cur >= 249 && *cur <= 252) *cur = 'u';
+                    if (*cur >= 218 && *cur <= 220) *cur = 'U';
+
+                    if (*cur == 231) *cur = 'c';
+                    if (*cur == 199) *cur = 'C';
+                }
+            }*/
+
+            return new string(chars);
+        }
     }
 
 }
