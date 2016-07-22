8ee4b074119ee1233833238216003bef290f7398
Leonardo Santos <devleeo@outlook.com>
2016-07-22 11:00:33 -0200

    Add TellRaw command

---
+++ src/Commands/CommandTellRaw.cs
@@ -0,0 +1,60 @@
+/*
+ *  This file is part of uEssentials project.
+ *      https://uessentials.github.io/
+ *
+ *  Copyright (C) 2015-2016  Leonardosc
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System.Collections.Generic;
+using Essentials.Api;
+using Essentials.Api.Command;
+using Essentials.Api.Command.Source;
+using Essentials.Api.Unturned;
+using Essentials.Common;
+using Essentials.Common.Util;
+using UnityEngine;
+using static Essentials.Commands.MiscCommands;
+
+namespace Essentials.Commands {
+
+    [CommandInfo(
+        Name = "tellraw",
+        Description = "Send raw message to an player.",
+        Usage = "[player/*console*] [message]",
+        MinArgs = 2
+    )]
+    public class CommandTellRaw : EssCommand {
+
+        public override CommandResult OnExecute(ICommandSource src, ICommandArgs args) {
+            var msg = args.Length == 2 ? args[1].ToString() : args.Join(1);
+            var color = ColorUtil.HasColor(msg) ? ColorUtil.GetColorFromString(ref msg) : Color.green;
+
+            if (args[0].Is("*console*")) {
+                UEssentials.ConsoleSource.SendMessage(msg, color);
+            } else {
+                if (!args[0].IsValidPlayerName) {
+                    return CommandResult.Lang("PLAYER_NOT_FOUND", args[0]);
+                }
+                args[0].ToPlayer.SendMessage(msg, color);
+            }
+
+            return CommandResult.Success();
+        }
+
+    }
+
+}
