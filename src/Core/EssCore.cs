8a94680b1846de8d797c4df0f1be9290b9bf1b63
Leonardo Santos <devleeo@outlook.com>
2016-12-16 16:39:20 -0200

    1.3.2.0

--- src/Core/EssCore.cs
+++ src/Core/EssCore.cs
@@ -63,8 +63,8 @@ namespace Essentials.Core {
     public sealed class EssCore : RocketPlugin {
 
         internal const string ROCKET_VERSION = "4.9.3.0";
-        internal const string UNTURNED_VERSION = "3.17.8.0";
-        internal const string PLUGIN_VERSION = "1.3.1.6";
+        internal const string UNTURNED_VERSION = "3.17.9.1";
+        internal const string PLUGIN_VERSION = "1.3.2.0";
 
 #if EXPERIMENTAL
         internal const string BUILD_INFO = " experimental (commit: $COMMIT_HASH$)";
