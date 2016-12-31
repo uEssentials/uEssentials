afa2f374166b0a1d8a81c2a6013d218d56ac1f2d
Leonardo Santos <devleeo@outlook.com>
2016-12-31 12:52:12 -0200

    Add scripts/

---
+++ scripts/update_lib.bat
@@ -0,0 +1,44 @@
+@ECHO OFF
+
+:: Copy from Unturned Directory, I wanna not redownload it again...
+SET "UNTURNED_ASM_FOLDER=C:\Program Files (x86)\Steam\steamapps\common\Unturned\Unturned_Data\Managed"
+
+:: Download latest rocket
+echo Downloading Rocket...
+wget "https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
+
+:: Create Temp dir
+IF NOT EXIST "TmpRocket" (
+  mkdir TmpRocket
+)
+
+:: Unzip Rocket
+7z -y x Rocket.zip -o./TmpRocket
+
+:: Copy Rocket dll's to lib\
+cp TmpRocket/Modules/Rocket.Unturned/*.dll ..\lib
+
+:: Copy Unturned dll's
+echo Copying unturned assemblies
+cp "%UNTURNED_ASM_FOLDER%\Assembly-CSharp.dll" ..\lib
+cp "%UNTURNED_ASM_FOLDER%\Assembly-CSharp-firstpass.dll" ..\lib
+cp "%UNTURNED_ASM_FOLDER%\UnityEngine.dll" ..\lib
+cp "%UNTURNED_ASM_FOLDER%\Newtonsoft.Json.dll" ..\lib
+echo Done!
+
+:: Clean Temp stuffs
+rmdir /s /q TmpRocket
+rm -f Rocket.zip
+
+git diff --name-only lib
+
+:: Commit changes
+set /P commit=Commit changes ? [Y/N] 
+
+IF /I "%commit%" EQU "Y" (
+  git add lib/
+  git commit -m "Update lib"
+  git push -u origin master
+)
+
+rem pause
