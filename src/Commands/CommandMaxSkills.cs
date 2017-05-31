0249fcb6ba940489ac9ec50f4088f960d6a22e0c
Leonardo Santos <devleeo@outlook.com>
2017-05-31 13:54:39 -0300

    remove extra spaces

--- src/Commands/CommandMaxSkills.cs
+++ src/Commands/CommandMaxSkills.cs
@@ -1,5 +1,4 @@
 ﻿#region License
-
 /*
  *  This file is part of uEssentials project.
  *      https://uessentials.github.io/
@@ -20,7 +19,6 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-
 #endregion
 
 using System.Linq;
