106fa3f1e4cff357d408ece0f0ac0c337cbdb951
Leonardo Santos <devleeo@outlook.com>
2018-03-27 11:05:23 -0300

    fix AviEconomy hook

--- src/Compatibility/Hooks/AviEconomyHook.cs
+++ src/Compatibility/Hooks/AviEconomyHook.cs
@@ -44,40 +44,35 @@ namespace Essentials.Compatibility.Hooks {
         private Type _bankType;
         private MethodInfo _getBalanceMethod;
         private MethodInfo _performPaymentMethod;
-        private MethodInfo _processPurchaseMethod;
 
         public AviEconomyHook() : base("avi_economy") { }
 
         public override void OnLoad() {
-
             UEssentials.Logger.LogInfo("Loading AviEconomy hook...");
 
             IRocketPlugin economyPlugin = R.Plugins.GetPlugins().FirstOrDefault(c => c.Name.EqualsIgnoreCase("AviEconomy"));
 
-            if (economyPlugin == null) { throw new Exception("AviEconomy Plugin couldn't be loaded..."); }
+            if (economyPlugin == null)
+                throw new Exception("AviEconomy Plugin couldn't be loaded...");
 
             this._bankType = economyPlugin.GetType().Assembly.GetType("com.aviadmini.rocketmod.AviEconomy.Bank");
 
-            if (this._bankType == null) { throw new Exception("AviEconomy Bank type couldn't be loaded..."); }
+            if (this._bankType == null)
+              throw new Exception("AviEconomy Bank type couldn't be loaded...");
 
             this._getBalanceMethod = ReflectUtil.GetMethod(this._bankType, "GetBalance", BindingFlags.Static | BindingFlags.Public,
                 new[] {typeof(string)});
 
-            if (this._bankType == null) { throw new Exception("AviEconomy GetBalance method couldn't be loaded..."); }
+            if (this._getBalanceMethod == null)
+                throw new Exception("AviEconomy GetBalance method couldn't be loaded...");
 
-            this._performPaymentMethod = ReflectUtil.GetMethod(this._bankType, "PerformPayment", BindingFlags.Static | BindingFlags.Public,
-                new[] {typeof(IRocketPlayer), typeof(string), typeof(decimal), typeof(string)});
+            this._performPaymentMethod = ReflectUtil.GetMethod(this._bankType, "PayAsServer", BindingFlags.Static | BindingFlags.Public,
+                new[] {typeof(IRocketPlayer), typeof(decimal), typeof(bool), typeof(decimal).MakeByRefType(), typeof(string)});
 
-            if (this._bankType == null) { throw new Exception("AviEconomy PerformPayment method couldn't be loaded..."); }
-
-            this._processPurchaseMethod = ReflectUtil.GetMethod(this._bankType, "ProcessPurchaseFromServer",
-                BindingFlags.Static | BindingFlags.Public,
-                new[] {typeof(UnturnedPlayer), typeof(decimal), typeof(string), typeof(bool)});
-
-            if (this._bankType == null) { throw new Exception("AviEconomy ProcessPurchase method couldn't be loaded..."); }
+            if (this._performPaymentMethod == null)
+                throw new Exception("AviEconomy PerformPayment method couldn't be loaded...");
 
             UEssentials.Logger.LogInfo("AviEconomy hook loaded.");
-
         }
 
         public override void OnUnload() { }
@@ -85,16 +80,17 @@ namespace Essentials.Compatibility.Hooks {
         public override bool CanBeLoaded() => R.Plugins.GetPlugins().Any(c => c.Name.EqualsIgnoreCase("AviEconomy"));
 
         public decimal Withdraw(UPlayer player, decimal amount) {
-
-            this._processPurchaseMethod.Invoke(this._bankType, new object[] {player.RocketPlayer, amount, null, true});
-
-            return this.GetBalance(player);
+            // args[3] = out decimal pFinalBalance
+            var args = new object[] { player.RocketPlayer, -amount, false, null, null };
+            this._performPaymentMethod.Invoke(this._bankType, args);
+            return (decimal) args[3];
         }
 
         public decimal Deposit(UPlayer player, decimal amount) {
-            this._performPaymentMethod.Invoke(this._bankType,
-                new object[] {new ConsolePlayer(), player.Id, amount, null});
-            return this.GetBalance(player);
+            // args[3] = out decimal pFinalBalance
+            var args = new object[] { player.RocketPlayer, amount, false, null, null };
+            this._performPaymentMethod.Invoke(this._bankType, args);
+            return (decimal) args[3];
         }
 
         public decimal GetBalance(UPlayer player) => (decimal) this._getBalanceMethod.Invoke(this._bankType, new object[] {player.Id});
